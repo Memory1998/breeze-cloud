0a1e09d1d1cb7fde0b54ce1e2267f7ebd93c2da5
breeze <breeze-cloud@foxmail.com>
2022-02-07 11:30:34 +0800

    网关过滤器

---
+++ breeze-gateway-server/src/main/java/com/breeze/cloud/gateway/config/GlobalFilter.java
@@ -0,0 +1,15 @@
+package com.breeze.cloud.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 拦截进行密码验证 和 验证码验证等
+ *
+ * @author breeze
+ * @date 2022-02-07
+ */
+@Slf4j
+public class GlobalFilter {
+
+
+}
