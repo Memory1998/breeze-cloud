2011676526efd0a09662980e99398832f854d6a8
breeze <breeze-cloud@foxmail.com>
2022-02-08 16:48:15 +0800

    网关过滤器

---
+++ breeze-gateway-server/src/main/java/com/breeze/cloud/gateway/config/LoginGlobalFilter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.breeze.cloud.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 拦截进行密码验证 和 验证码验证等
+ * RemoveRequestHeader
+ *
+ * @author breeze
+ * @date 2022-02-07
+ */
+@Slf4j
+@Component
+@Order(-1000)
+public class LoginGlobalFilter implements GlobalFilter {
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("执行了自定义的全局过滤器, {}", exchange.getRequest().getRemoteAddress());
+        String token = exchange.getRequest().getQueryParams().getFirst("Authorization");
+        if (token == null) {
+            log.error("");
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+}
