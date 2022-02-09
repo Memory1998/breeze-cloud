e3ac6a554237b22bffe3b9ea3195acd07c67726b
breeze <breeze-cloud@foxmail.com>
2022-02-09 08:50:57 +0800

    网关过滤器 验证码验证

--- breeze-gateway-server/src/main/java/com/breeze/cloud/gateway/config/GatewayGlobalFilter.java
+++ breeze-gateway-server/src/main/java/com/breeze/cloud/gateway/config/GatewayGlobalFilter.java
@@ -20,7 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -35,18 +34,12 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @Component
 @Order(-1000)
-public class LoginGlobalFilter implements GlobalFilter {
-
+public class GatewayGlobalFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("执行了自定义的全局过滤器, {}", exchange.getRequest().getRemoteAddress());
-        String token = exchange.getRequest().getQueryParams().getFirst("Authorization");
-        if (token == null) {
-            log.error("");
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
-        }
         return chain.filter(exchange);
     }
+
 }
